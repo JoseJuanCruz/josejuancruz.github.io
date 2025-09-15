03f0cb2b9b1037ca349b956ffa49d3db1fb26f91
JoseJuanCruz <cruzjj30@gmail.com>
2025-09-15 04:42:10 +0000

    Deploying to gh-pages from @ JoseJuanCruz/josejuancruz.github.io@475478a2514711bf96821804776a80225117374a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%63%72%75%7A%6A%6A%33%30@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/josejuancruz", "_blank");
         },
       },{
         id: 'social-inspire',
